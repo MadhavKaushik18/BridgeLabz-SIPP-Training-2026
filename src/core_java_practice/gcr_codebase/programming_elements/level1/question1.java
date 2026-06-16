b5c2a1b9249c3ad4f128631da0097fb4184b3e86
kaushik <madhavkaushik204216@gmail.com>
2026-06-16 09:05:12 +0530

    [Madhav Kaushik] Added programming_elements assignment

---
+++ src/core_java_practice/gcr_codebase/programming_elements/level1/question1.java
@@ -0,0 +1,12 @@
+package core_java_practice.gcr_codebase.programming_elements.level1;
+
+public class question1 {
+    public static void main(String[] args){
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age is: " + age);
+    }
+}
